69b7835de02ff31cafc733e8d42a7b3ce8d47234
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-03-27 21:15:12 -0400

    Configuring additional DataNodes using the existing local system.
    I'm configuring additional datanodes using existing hadoop configured in pseudo-distributed mode.

---
+++ configure_additionalDN.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# I am currently running hadoop on my local system in pseudo-distributed mode.
+# In order to configure multiple data nodes in our system,will make use of this shell script 
