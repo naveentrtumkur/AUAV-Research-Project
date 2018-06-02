dd2a3215116008ec96a388db0a41dfdcce142873
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-06-01 21:35:42 -0400

    Feature extraction

--- Feature_extraction/example.py
+++ Feature_extraction/example.py
@@ -1,5 +1,5 @@
 res=[]
-with open('JSONfile22.json','rt') as myfile:
+with open('JSONfile58.json','rt') as myfile:
     for line in myfile:
         #print(line)
         res.append(line.strip("{}"))
@@ -18,15 +18,66 @@ for char in path_taken:
 
 print(pos)
 
+y_dist = 0
 # check whether our list contains 'UP' or 'DW'
-if 'UP' in pos:
+if 'U'in pos and 'P' in pos:
     print("UP is present")
-elif 'DW' in pos:
+    y_dist = 1
+    #pos.strip("UP")
+    [s.strip('U') for s in pos]
+    [s.strip('P') for s in pos]
+elif "DW" in pos:
     print("DW is present")
+    y_dist = -1
+    #pos.strip("DW")
 else:
-    print("Both not present")
+    print("Drone in central slice")
 
-    
+mylist = pos
+mylist = ' '.join(mylist).replace('U','').replace('P','').replace('_','').split()
+#mylist = ' '.join(mylist).replace('P','').split()
+print(mylist)
 
+pos =mylist
+
+'''for char in pos:
+    for case in switch(char):
+        if case('C'):
+            print('C')
+            break
+        if case('L'):
+            print('L')
+        if case(): # This is like a default case
+            print("This is something else")   
+'''
+print(len(pos))
+n = len(pos)
+
+print(pos[n-1])
+cur_pos = pos[n-1]
+
+cube= []
+if cur_pos == 'C':
+    cube.append(1)
+    cube.append(1+y_dist)
+    cube.append(1)
+elif cur_pos == 'L':
+    cube.append(0)
+    cube.append(1+y_dist)
+    cube.append(1)
+elif cur_pos == 'B':
+    cube.append(0)
+    cube.append(1+y_dist)
+    cube.append(0)
+elif cur_pos == 'R':
+    cube.append(1)
+    cube.append(1+y_dist)
+    cube.append(0)
+elif cur_pos == 'F':
+    cube.append(1)
+    cube.append(1+y_dist)
+    cube.append(1)
+
+print(cube)
 #for elem in res:
  ##   print(elem)  
